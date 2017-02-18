bbbe4a8c2632b055fa681ebd2e5e46b486625491
edatrix <lespath@gmail.com>
2017-02-18 15:28:38 -0700

    more setup

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,26 @@
+# # install and require all dependencies
+# require 'sinatra/base'
+# require 'sprockets'
+# # require 'uglifier'
+# # require 'sass'
+
 class DoSomethingFfs < Sinatra::Base
+  # # initialize new sprockets environment
+  # environment = Sprockets::Environment.new
+
+  # # append assets paths
+  # environment.append_path "assets/stylesheets"
+  # environment.append_path "assets/javascripts"
+
+  # # compress assets
+  # environment.js_compressor  = :uglify
+  # environment.css_compressor = :scss
+
+  # # get assets
+  # get "/assets/*" do
+  #   env["PATH_INFO"].sub!("/assets", "")
+  #   settings.environment.call(env)
+  # end
 
   get '/' do
     erb :index
