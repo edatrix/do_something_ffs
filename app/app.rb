1c4b69cd35172cdb1c53cd949d15a154260867cb
edatrix <lespath@gmail.com>
2017-02-19 20:24:34 -0700

    images

--- app/app.rb
+++ app/app.rb
@@ -1,26 +1,4 @@
-# # install and require all dependencies
-# require 'sinatra/base'
-# require 'sprockets'
-# # require 'uglifier'
-# # require 'sass'
-
 class DoSomethingFfs < Sinatra::Base
-  # # initialize new sprockets environment
-  # environment = Sprockets::Environment.new
-
-  # # append assets paths
-  # environment.append_path "assets/stylesheets"
-  # environment.append_path "assets/javascripts"
-
-  # # compress assets
-  # environment.js_compressor  = :uglify
-  # environment.css_compressor = :scss
-
-  # # get assets
-  # get "/assets/*" do
-  #   env["PATH_INFO"].sub!("/assets", "")
-  #   settings.environment.call(env)
-  # end
 
   get '/' do
     erb :index
@@ -32,6 +10,10 @@ class DoSomethingFfs < Sinatra::Base
 
   get '/burned-out' do
     erb :burned_out
+  end
+
+  get '/i-can-help' do
+    erb :help
   end
   
 end
